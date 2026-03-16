e47f7cf6e63b2d3c513b55b41045bf485e16e3bc
Andrea Botello <abotellod@gmail.com>
2026-03-16 18:02:46 -0500

    Temas

--- src/config/global.js
+++ src/config/global.js
@@ -36,22 +36,94 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Trazado de patrones (<em>software</em> de patronaje)',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Personalizar área de trabajo - Optitex',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Trazo de la falda - Optitex',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Trazo de la base superior femenina - Optitex',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo: 'Traslados de pinza - Optitex',
+            hash: 't_1_4',
+          },
+          {
+            numero: '1.5',
+            titulo: 'Cortes base superior femenina - Optitex',
+            hash: 't_1_5',
+          },
+          {
+            numero: '1.6',
+            titulo: 'Blusa hasta la cadera - Optitex',
+            hash: 't_1_6',
+          },
+          {
+            numero: '1.7',
+            titulo: 'Cortes princesa y francés - Optitex',
+            hash: 't_1_7',
+          },
+          {
+            numero: '1.8',
+            titulo: 'Modelos de faldas - Optitex',
+            hash: 't_1_8',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Trazo y corte manual',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Reconocimiento de las prendas',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Concepto de trazo y corte',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Proceso de extendido',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo: 'Tipos de trazo',
+            hash: 't_2_4',
+          },
+          {
+            numero: '2.5',
+            titulo: 'Tendido, marcación y corte',
+            hash: 't_2_5',
+          },
+          {
+            numero: '2.6',
+            titulo: 'Características del material',
+            hash: 't_2_6',
+            desarrolloContenidos: true,
+          },
+          {
+            numero: '2.7',
+            titulo: 'Terminología industrial de trazo y corte',
+            hash: 't_2_7',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -85,7 +157,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF4_524537_DU.pdf',
       },
       {
         icono: 'fas fa-download',
@@ -101,22 +173,159 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: 'Sitio web',
-      link: '',
+      tema: 'Personalizar área de trabajo.',
+      referencia: 'Araque, J. (13 de noviembre de 2013). Tiqueteador de tela.',
+      tipo: 'Video',
+      link: 'https://youtu.be/uO2HGq24lkE',
+    },
+    {
+      tema: 'Reconocimiento de las prendas.',
+      referencia:
+        'Digitalmentes asesorías. (28 de junio de 2020). Manipulando pinzas en  Optitex.',
+      tipo: 'Video',
+      link: 'https://youtu.be/7X9G8DTMV9w',
+    },
+    {
+      tema: 'Trazo y corte manual.',
+      referencia:
+        'Duarte, N. (1983). Conocimientos básicos de corte. Unidad instruccional No. 3. Servicio Nacional de Aprendizaje SENA.',
+      tipo: 'Libro',
+      link: 'downloads/modisteria-principios-basicos-de-corte.pdf',
+    },
+    {
+      tema: 'Trazado de patrones.',
+      referencia:
+        'Gutiérrez, L (2011). Como graduar los patrones básicos de dama.',
+      tipo: 'Video',
+      link: 'https://youtu.be/amafgzhmMlw',
+    },
+    {
+      tema: 'Trazado de patrones.',
+      referencia:
+        'Herramientas digitales Moda. (13 de junio 2018). AUDACES TIZADA Trazo.',
+      tipo: 'Video',
+      link: 'https://youtu.be/C0O6derJC2s',
+    },
+    {
+      tema: 'Trazado de patrones.',
+      referencia:
+        'Herramientas digitales Moda. (15 de marzo 2018). OPTITEX - MARKER Trazo de patrones.',
+      tipo: 'Video',
+      link: 'https://youtu.be/fscxWYsenLE',
+    },
+    {
+      tema: 'Trazado de patrones.',
+      referencia:
+        'Herramientas digitales Moda. (2 de mayo 2018). OPTITEX Falda Básica.',
+      tipo: 'Video',
+      link: 'https://youtu.be/oBVuhFGidN4',
+    },
+    {
+      tema: 'Trazado de patrones.',
+      referencia:
+        'Lisandro, L. (24, de octubre de 2020). Cómo crear pinzas en Optitex.',
+      tipo: 'Video',
+      link: 'https://youtu.be/DX9-gvA37e4',
+    },
+    {
+      tema: 'Trazado de patrones.',
+      referencia: 'Londoño, A. (31 de marzo de 2020). Corpiño.',
+      tipo: 'Video',
+      link: 'https://youtu.be/yUYtfsOZoSg',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: '<em>Marker</em>',
+      significado:
+        'Programa de diseño de corte, que permite la optimización del uso de textiles, a través de anidamiento automatizado o ubicación manual de piezas en la mesa de trazo.',
+    },
+    {
+      termino: 'Optitex',
+      significado:
+        'Herramienta de diseño de patrones, muy utilizada en la industria textil, en empresas dedicadas a la confección de prendas de vestir.',
+    },
+    {
+      termino: 'PDS',
+      significado: 'Se traduce como sistema de diseño de patrones.',
+    },
+    {
+      termino: 'Piquetes',
+      significado:
+        'Línea corta (0,3 cm máximo 0,5 cm) en dirección perpendicular al molde, señaladas en el patrón, con el fin de facilitar la operación de confección, al indicar ubicaciones o al guiar y al unir cortes.',
+    },
+    {
+      termino: 'Procedimiento',
+      significado:
+        'Descripción precisa de los pasos para realizar un proceso. Es la respuesta al cómo hacer el proceso.',
+    },
+    {
+      termino: 'Proceso',
+      significado:
+        'Descripción general de los pasos de una actividad o conjunto de operaciones.',
+    },
+    {
+      termino: 'Talla',
+      significado:
+        'Expresión normalizada que permite identificar a las personas para el uso de prendas de vestir (Vélez et al., 1996).',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Araque, J. (13 de noviembre de 2013). Tiqueteador de tela. [video]. Youtube.',
+      link: 'https://youtu.be/uO2HGq24lkE',
+    },
+    {
+      referencia:
+        'Digitalmentes asesorías. (28 de junio de 2020). Manipulando pinzas en Optitex. [video]. Youtube.',
+      link: 'https://youtu.be/7X9G8DTMV9w',
+    },
+    {
+      referencia:
+        'Duarte, N. (1983). Conocimientos básicos de corte. Unidad instruccional No. 3. Servicio Nacional de Aprendizaje SENA.',
+      link: 'https://repositorio.sena.edu.co/handle/11404/1432?show=full',
+    },
+    {
+      referencia:
+        'Evalds, P. (13 de Julio 2019). Máquina fusionadora. [video]. Youtube.',
+      link: 'https://youtu.be/56wpJlyR_v4',
+    },
+    {
+      referencia:
+        'Gutiérrez, L., Moncayo, A., Tanaka, K., Kimura, F., Moreno, D. (2011).  Manual de patronaje básico e interpretación de diseños. Servicio Nacional de Aprendizaje SENA.',
+      link:
+        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/q6j6k0/sena_aleph000025496',
+    },
+    {
+      referencia:
+        'Herramientas digitales Moda. (13 de junio 2018). AUDACES TIZADA Trazo. [video]. Youtube.',
+      link: 'https://youtu.be/C0O6derJC2s',
+    },
+    {
+      referencia:
+        'Herramientas digitales Moda. (15 de marzo 2018). OPTITEX - MARKER Trazo de patrones. [video]. Youtube.',
+      link: 'https://youtu.be/fscxWYsenLE',
+    },
+    {
+      referencia:
+        'Herramientas digitales Moda. (2 de mayo 2018). Falda Basica. [video]. Youtube.',
+      link: 'https://youtu.be/oBVuhFGidN4',
+    },
+    {
+      referencia:
+        'Lisandro, L. (24, de octubre de 2020). Cómo crear pinzas en Optitex. [video]. Youtube.',
+      link: 'https://www.youtube.com/watch?v=DX9-gvA37e4&feature=youtu.be',
+    },
+    {
+      referencia:
+        'Londoño, A. (31 de marzo de 2020). Corpiño. [video]. Youtube',
+      link: 'https://youtu.be/yUYtfsOZoSg',
+    },
+    {
+      referencia:
+        'Vélez, M., García M., Hincapié L. (1996).  Patronaje y escalado línea interior y deportiva.  Servicio Nacional de Aprendizaje SENA.',
+      link: 'https://repositorio.sena.edu.co/handle/11404/4811',
     },
   ],
   creditos: [
@@ -124,14 +333,14 @@ export default {
       titulo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre completo',
+          nombre: 'Claudia Johanna Gómez Pérez',
           cargo: 'Responsable del ecosistema',
           centro: 'Dirección General',
         },
         {
-          nombre: 'Nombre completo',
+          nombre: 'Edison Eduardo Mantilla Cuadros',
           cargo: 'Responsable de línea de producción',
-          centro: 'Centro XYZ - Regional XYZ',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
     },
@@ -139,9 +348,27 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Lizeth Maritza Rodríguez Beltrán',
+          cargo: 'Experta temática',
+          centro:
+            'Centro de Manufactura en Textil y Cuero - Regional Distrito Capital',
+        },
+        {
+          nombre: 'Natalia Andrea Bueno Pizarro',
+          cargo: 'Diseñadora instruccional y evaluadora instruccional',
+          centro:
+            'Centro para la Industria de la Comunicación Gráfica - Distrito Capital',
+        },
+        {
+          nombre: 'Julia Isabel Roberto',
+          cargo: 'Diseñadora instruccional y evaluadora instruccional',
+          centro:
+            'Centro para la Industria de la Comunicación Gráfica - Distrito Capital',
+        },
+        {
+          nombre: 'Sandra Paola Morales Páez',
+          cargo: 'Evaluador instruccional',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
     },
@@ -149,19 +376,19 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Diseñador de contenidos',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Yuly Andrea Rey Quiñonez',
+          cargo: 'Diseñadora de contenidos',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Desarrollador <i>full stack</i>',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Andrea Paola Botello De la Rosa',
+          cargo: 'Desarrolladora <em>full stack</em>',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Animador y productor audiovisual',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Maria Alejandra Vera Briceño',
+          cargo: 'Animadora y productora multimedia',
+          centro: 'Regional Santander - Centro Agroturístico',
         },
       ],
     },
@@ -169,14 +396,14 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Validador y vinculador de recursos educativos digitales',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Yineth Ibette González Quintero',
+          cargo: 'Validadora de recursos educativos digitales',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Evaluador de contenidos inclusivos y accesibles',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Erika Fernanda Mejía Pinzón',
+          cargo: 'Evaluadora para contenidos inclusivos y accesibles',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
     },
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,11 +9,12 @@
           i.fas.fa-info
       h1 Introducción
         
-    p.mb-5  En el siguiente video, los aprendices tendrán un acercamiento al contenido del componente, los temas y conceptos tratados y la manera en la que serán abordados.
+    p.mb-4  En el siguiente video, los aprendices tendrán un acercamiento al contenido del componente, los temas y conceptos tratados y la manera en la que serán abordados.
+    
     figure
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/TlMAZzjg3Ok" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Introducción - #[em Software] Optitex.  
+      figcaption Video. Introducción - #[em Software] Optitex.  
 
 </template>
 
